71efa21ec6665eca0787d43f16bef606f6800820
Rosnec <rosnec.dev@gmail.com>
2014-08-30 16:34:36 -0400

    Wrote a script to take Lisp sessions text files and wrap them in markdown
    Wrote said script to automatically make a syntax highlighted version of
    the Lisp sessions, along with a link at the top to the plain text file.

---
+++ scripts/mdify_txt.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+from argparse import ArgumentParser
+from shutil import copyfileobj
+from sys import stdout
+
+def get_args():
+    parser = ArgumentParser()
+
+    parser.add_argument("file", metavar="File",
+        help="markdown file")
+    parser.add_argument("title", metavar="Title", default="", nargs='?',
+        help="title for markdown page")
+    parser.add_argument("--layout", default="default",
+        help="layout for markdown page")
+
+    return parser.parse_args()
+
+def main():
+    args = get_args()
+
+    str1 = """\
+---
+title: {}
+layout: {}
+---
+[Download]({})
+
+```CommonLisp""".format(args.title, args.layout, args.file)
+    str2 = "```\n"
+
+    print(str1)
+    with open(args.file, "r") as f:
+        copyfileobj(f, stdout)
+    print(str2)
+
+if __name__ == "__main__":
+    main()
